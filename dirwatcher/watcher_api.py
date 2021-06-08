5d7e0231b37feb8d80dd05d9ee5c40b303e4d344
Krzysztof Wiśniewski <krzysztof.wisniewski@codility.com>
2021-06-08 23:41:06 +0200

    Add save_current_state endpoint

---
+++ dirwatcher/watcher_api.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import logging
+from flask import Flask
+
+from dirwatcher.infrastructure.checkpoint_store import CheckpointStoreAdapter
+from dirwatcher.infrastructure.hasher import Hasher
+from dirwatcher.infrastructure.traverser import make_traverser
+from dirwatcher.watcher_service import WatcherService, NoPriorCheckpointSavedError, InvalidDirectoryRequested
+
+app = Flask("dirwatcher")
+logger = logging.getLogger(__name__)
+STORE_LOCATION = "store.json"
+
+
+@app.route("/<path:directory>/save", methods=["POST"])
+def save_current_state(directory):
+    service = WatcherService(make_traverser(Path(directory)), CheckpointStoreAdapter(), Hasher())
+    try:
+        service.checkpoint_current_state()
+    except InvalidDirectoryRequested as e:
+        logger.error(e)
+        return {"error": "the directory you requested does not exist"}, 400
+    return "OK", 200
