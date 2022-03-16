9855f47fdaa1fbe3d84ef0f69980311628a4f455
Kamil <12999736+camaeel@users.noreply.github.com>
2022-03-16 15:46:46 +0100

    finished task

--- dirwatcher/infrastructure/checkpoint_store.py
+++ dirwatcher/infrastructure/checkpoint_store.py
@@ -1,10 +1,27 @@
+import json
 from pathlib import Path
 
 STORE_LOCATION = "store.json"
 
+def mapEntry(k, v):
+    return Path(k), v
 
 def load_checkpoints() -> dict[Path, str]:
-    ...
+    result = {}
+
+    f = None
+    try: 
+        f = open(STORE_LOCATION)
+        # Do something with the file
+        jsonContent = json.load(f)
+            
+        for k in jsonContent:
+            result[Path(k)]=jsonContent[k]
+    finally:
+        if f:
+            f.close()
+    return result
+
 
 
 def save_checkpoints(hashes: dict[Path, str]):
